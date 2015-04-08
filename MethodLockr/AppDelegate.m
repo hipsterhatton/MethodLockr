f509c8bf7bcf6fcbf87071a6a21491a68126c0a4
Stephen Hatton <Stephen@Stephens-MacBook-Pro.local>
2015-04-08 20:03:37 +0100

    Initial Commit

---
+++ MethodLockr/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  MethodLockr
+//
+//  Created by Stephen Hatton on 08/04/2015.
+//  Copyright (c) 2015 Stephen Hatton. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
